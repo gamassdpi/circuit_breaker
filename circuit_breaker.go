8537acb19cb1df9b2ee4360530f208a20779b899
gamassdpi <gamas.namara@durian.money>
2026-05-01 19:54:36 +0700

    feat: scaffold CircuitBreaker with stub implementation

--- circuit_breaker.go
+++ circuit_breaker.go
@@ -14,3 +14,22 @@ type CircuiBreaker interface {
 	RecordFailure() error
 	RecordSuccess() error
 }
+
+type circuitBreaker struct {
+	state        CircuitBreakerState
+	failCount    int
+	successCount int
+	threshold    int
+}
+
+func NewCircuitBreaker(threshold int) CircuiBreaker {
+	return &circuitBreaker{threshold: threshold}
+}
+
+func (cb *circuitBreaker) Execute() error { return nil }
+
+func (cb *circuitBreaker) CurrentState() CircuitBreakerState { return Closed }
+
+func (cb *circuitBreaker) RecordFailure() error { return nil }
+
+func (cb *circuitBreaker) RecordSuccess() error { return nil }
