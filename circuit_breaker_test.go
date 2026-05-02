23686b32c37a4c5dc74b98790f7a30f402b38b99
gamassdpi <gamas.namara@durian.money>
2026-05-02 18:48:05 +0700

    test: add unit test for Allow when Open and timeout not expired

--- circuit_breaker_test.go
+++ circuit_breaker_test.go
@@ -1,1 +1,21 @@
-package circuitbreaker_test
+package circuitbreaker
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAllow_ShouldReturnFalse_WhenOpen_AndTimeoutNotExpired(t *testing.T) {
+	baseTime := time.Now()
+	cb := &circuitBreaker{
+		now: func() time.Time { return baseTime },
+		state: Open,
+		openedAt: baseTime,
+		timeout: 30 * time.Second,
+	}
+
+	baseTime = baseTime.Add(29 * time.Second)
+	assert.Equal(t, false, cb.Allow())
+}
