fa1f4eaaf142036c753e9f85c7efa5e6039191be
gamassdpi <gamas.namara@durian.money>
2026-05-03 15:56:14 +0700

    test: add unit test for allow when state transition and cooldown behaviour

--- circuit_breaker_test.go
+++ circuit_breaker_test.go
@@ -1,6 +1,7 @@
 package circuitbreaker
 
 import (
+	// "fmt"
 	"testing"
 	"time"
 
@@ -10,9 +11,9 @@ import (
 func TestNewCircuitBreaker_InitialState(t *testing.T) {
 	failureThreshold := 3
 	cooldownPeriod := 30 * time.Second
-	baseTime := time.Now
+	baseTime := time.Now()
 
-	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, baseTime)
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
 
 	assert.Equal(t, Closed, cb.CurrentState())
 	assert.Equal(t, 0, cb.GetFailureCount())
@@ -22,9 +23,9 @@ func TestNewCircuitBreaker_InitialState(t *testing.T) {
 func TestAllow_WhenClosed_ReturnsTrue(t *testing.T) {
 	failureThreshold := 3
 	cooldownPeriod := 30 * time.Second
-	baseTime := time.Now
+	baseTime := time.Now()
 
-	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, baseTime)
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
 
 	assert.Equal(t, true, cb.Allow())
 }
@@ -32,15 +33,66 @@ func TestAllow_WhenClosed_ReturnsTrue(t *testing.T) {
 func TestRecordFailure_BelowThreshold_StaysClosed(t *testing.T) {
 	failureThreshold := 3
 	cooldownPeriod := 30 * time.Second
-	baseTime := time.Now
+	baseTime := time.Now()
 
-	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, baseTime)
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
 
 	cb.RecordFailure()
 	cb.RecordFailure()
 
 	assert.Equal(t, 2, cb.GetFailureCount())
 	assert.Equal(t, Closed, cb.CurrentState())
+}
+
+func TestRecordFailure_ReachesTreshold_TransitionsToOpen(t *testing.T) {
+	failureThreshold := 3
+	cooldownPeriod := 30 * time.Second
+	baseTime := time.Now()
+
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
+
+	cb.RecordFailure()
+	cb.RecordFailure()
+	cb.RecordFailure()
+
+	assert.Equal(t, Open, cb.CurrentState())
+}
+
+func TestAllow_WhenOpen_BeforeCooldown_ReturnsFalse(t *testing.T) {
+	failureThreshold := 3
+	cooldownPeriod := 30 * time.Second
+	baseTime := time.Now()
+
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
+
+	// trigger state transitions to Open
+	cb.RecordFailure()
+	cb.RecordFailure()
+	cb.RecordFailure()
+
+	// add time before cooldown period
+	baseTime = baseTime.Add(29 * time.Second)
+
+	assert.Equal(t, false, cb.Allow())
+}
+
+func TestAllow_WhenOpen_AfterCooldown_TransitionToHalfOpen_ReturnTrue(t *testing.T) {
+	failureThreshold := 3
+	cooldownPeriod := 30 * time.Second
+	baseTime := time.Now()
+
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, func() time.Time { return baseTime })
+
+	// trigger state transitions to Open
+	cb.RecordFailure()
+	cb.RecordFailure()
+	cb.RecordFailure()
+
+	// add time before cooldown period
+	baseTime = baseTime.Add(31 * time.Second)
+	// fmt.Println(cb.IsProbeInFlight()) // false
+	assert.Equal(t, true, cb.Allow())
+	assert.Equal(t, HalfOpen, cb.CurrentState())
 }
 
 func TestAllow_ShouldReturnFalse_WhenOpen_AndTimeoutNotExpired(t *testing.T) {
