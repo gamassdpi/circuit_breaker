066b265542d2dae8d2f8eebba424d222be549eab
gamassdpi <gamas.namara@durian.money>
2026-05-03 11:54:31 +0700

    test: add test for circuitBreaker new constructor method

--- circuit_breaker_test.go
+++ circuit_breaker_test.go
@@ -7,6 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNewCircuitBreaker(t *testing.T) {
+	failureThreshold := 3
+	cooldownPeriod := 30 * time.Second
+	baseTime := time.Now
+
+	cb := NewCircuitBreaker(failureThreshold, cooldownPeriod, baseTime)
+
+	assert.Equal(t, Closed, cb.CurrentState())
+	assert.Equal(t, 0, cb.GetFailureCount())
+	assert.Equal(t, false, cb.IsProbeInFlight())
+}
+
 func TestAllow_ShouldReturnFalse_WhenOpen_AndTimeoutNotExpired(t *testing.T) {
 	baseTime := time.Now()
 	cb := &circuitBreaker{
